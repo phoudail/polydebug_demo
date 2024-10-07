9fcbe48a2dfd35c37824cbc1905a8596d8a5d669
phoudail <philemon.houdaille@irisa.fr>
2024-10-07 10:49:43 +0200

    Initial commit

---
+++ vscode-polydebug/sampleWorkspace/test.py
@@ -0,0 +1,9 @@
+x = 42
+
+a = 12
+
+y = polyglotEval("js", "/run/media/phoudail/homedir/phoudail/Code/polyglot_DAP/vscode-mock-debug/sampleWorkspace/test2.js")
+
+z = a + x
+
+boom = y + z
---
+++ vscode-polydebug/sampleWorkspace/test2.js
@@ -0,0 +1,5 @@
+var foo = 2;
+var bar = 40;
+var t = bar + foo;
+
+module.exports = t
